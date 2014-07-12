8811e3407eaa8c8667893d48f9547107d539c9d5
Viktor Nonov <vnonov@gmail.com>
2014-07-12 14:35:26 -0400

    sets rvm path before gemset creation

--- create-rails-app.sh
+++ create-rails-app.sh
@@ -5,15 +5,29 @@
 command -v rvm >/dev/null 2>&1 || { echo >&2 "I require 'rvm' but it's not installed.  Aborting."; exit 1; }
 command -v gem >/dev/null 2>&1 || { echo >&2 "I require 'gem' but it's not installed.  Aborting."; exit 1; }
 
-mkdir $1
-cd $1
+# Load RVM into a shell session *as a function*
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm"
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  source "/usr/local/rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+fi
 
+echo Marking dir $1
+mkdir $1
+cd MarkTwo
+
+echo Creating gemset $1-gems
 rvm gemset create $1-gems
 rvm gemset use $1-gems
+
 cd .
+echo Installing rails
 gem install rails
+
+echo Creating new rails application
 rails new $1
-cd $1
 
 rvm gemset list
 
